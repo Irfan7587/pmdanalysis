8dddd8972a55838df7740738a17c003e81b7dd56
Irfan7587 <112637982+Irfan7587@users.noreply.github.com>
2025-03-11 00:37:06 -0500

    Delete CashPayment.java

--- CashPayment.java
+++
@@ -1,7 +0,0 @@
-public class CashPayment {
-    public double processCash(double amount, double bill) {
-        System.out.println("Accepting cash: $" + amount);
-        double bal = amount - bill;
-        return Math.round(bal * 100.0) / 100.0;
-    }
-}
